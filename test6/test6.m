ec323eb6489ac1eeabd60ba62dbcf14214e1315b
PourRevenir <yyk39693232@outlook.com>
2024-05-19 23:26:50 +0800

    19 MAY 23:27 commit

--- test6/test6.m
+++ test6/test6.m
@@ -2,8 +2,8 @@
 clear;
     
 % 定义参数
-x = 0:0.1:1;
-t = 0:0.1:1;
+x = 0:0.01:1;
+t = 0:0.001:0.014;
 [X,T]=meshgrid(x,t);
 
 uxt = 3*sin(5*pi*X).*exp(-(5*pi)^2*T)+100;
@@ -14,3 +14,5 @@ contourf(X,T,uxt,20);
 colorbar;
 xlabel('x');
 ylabel('t');
+
+surf(x,t,uxt);
